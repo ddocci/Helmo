1d01611625e19ec0ac06868e23888378e137ea52
Leestrange <l95668140@gmail.com>
2025-08-05 11:27:29 +0900

    오전 최종 수정본

--- helmoF/src/App.jsx
+++ helmoF/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Button } from './components/Button'
-import {Routes,Route,Link,useNavigate} from "react-router-dom"
+import {Routes,Route,Link,useNavigate, BrowserRouter, Router} from "react-router-dom"
 import Login from './pages/Login'
 
 import Workmain from './pages/Workmain'
 import Adminmain from './pages/Adminmain'
 import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
+import Statistics from './pages/Statistics'
+import Retouch from './pages/Retouch'
 
 // import './App.css'
 
@@ -19,8 +21,10 @@ function App() {
       <Route path="/" element={<Login/>}/>
       <Route path="/workmain/:id" element={<Workmain/>}/>
       <Route path="/adminmain/:id" element={<Adminmain/>}/>
-      <Route path="/edit" element={<Edit/>}/>
+      <Route path="/edit/:date" element={<Edit/>}/>
       <Route path="/*" element={<Notfound/>}/>
+      <Route path="/statistics" element={<Statistics/>}/>
+      <Route path="/retouch/:date" element={<Retouch/>}/>
     </Routes>
     </>
   )
---
+++ helmoF/src/pages/Statistics.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Statistics = () => {
+  return (
+    <div>통계통계통계통계 페이지</div>
+  )
+}
+
+export default Statistics
