ea292658515890cd404dd125fa0288d6f4136500
1q2w3e4r123 <183115@jnu.ac.kr>
2025-08-09 15:14:36 +0900

    수정: 라우터에 관리자 주간/월간 통계 조회 추가

--- helmoB/routes/adminRouter.js
+++ helmoB/routes/adminRouter.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/authToken");
-const { getDailyScore } = require("../controllers/dailyScoreController")
+const { getDailyScore } = require("../controllers/dailyScoreController");
+const { getWeeklyStatistics } = require("../controllers/weeklyStatController");
+const { getMonthlyStatistics } = require("../controllers/monthlyStatController");
 
 // /api/admin/score-daily?date=0000-00-00
 // 관리자 일일 점수
 router.get("/score-daily", verifyToken(["admin"]), getDailyScore);
+
+// 관리자 주간 통계
+router.get("/weekly-stat", verifyToken(["admin"]), getWeeklyStatistics);
+
+// 관리자 월간 통계
+router.get("monthly-stat", verifyToken(["admin"]), getMonthlyStatistics);
 
 module.exports = router;
